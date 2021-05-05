b18fb74171c6c2c03393992f9fae49fe7fc385ff
TuomoKalliokoski <tkalliok@gmail.com>
2021-05-05 14:34:07 +0300

    Required scripts to interface with ChemProp

---
+++ ml_chemprop_pred.sh
@@ -0,0 +1,2 @@
+chemprop_predict --test_path $1 --checkpoint_dir $2 --preds_path $3
+
---
+++ ml_chemprop_train.sh
@@ -0,0 +1,1 @@
+chemprop_train --target_columns docking_score --data_path $1 --separate_val_path $2 --separate_test_path $3 --dataset_type regression --save_dir $4
